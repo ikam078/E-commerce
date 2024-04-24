806288f7efd37bc1637969728a537aa40bb63f3e
ilham_ath <ilham.athallah08@gmail.com>
2024-04-24 14:58:32 +0700

    new

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,17 +25,15 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected function redirectTo ()
+    protected function redirectTo()
     {
-        if (auth()->user()->role == 'admin')
-        {
+        if (auth()->user()->role == 'admin') {
             return '/admin/dashboard';
-        } else if (auth()->user()->role == 'user')
-        {
+        } else if (auth()->user()->role == 'user') {
             return '/user/dashboard';
         }
     }
-    
+
 
     /**
      * Create a new controller instance.
